7e4b6993d14784a005c1199b9fbd54467d5201b6
Service Web <sw@gssmail.com>
2024-03-14 11:28:56 -0500

    Commit changes in C:\Websites\Git\emarlow\5530\GCG_5530_Warranty.sql

---
+++ GCG_5530_Warranty.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_5530_Warranty
+ * ========================================== */
+CREATE TABLE "GCG_5530_Warranty"(
+ "Warranty_Code" CHAR(20),
+ "Terms" NUMERIC(8,0));
+
+
